1b0861c8bf66a0dee3e62e1bf16e92a22600fb68
DaZuo <Jaly201319@hotmail.com>
2018-02-08 18:18:44 +0800

    continue reading chapter 3

--- the-python-tutorial/3.an_informal_introducation_to_python.py
+++ the-python-tutorial/3.an_informal_introducation_to_python.py
@@ -49,3 +49,10 @@ print('un' * 3 + 'ium')
 
 # 输出 Python
 print('Py'    'thon')
+
+# Fibonacci series:
+# the sum of two elements defines the next
+a, b = 0, 1
+while b < 10:
+    print(b)
+    a, b = b, a + b
